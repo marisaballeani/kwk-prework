528c614ab79e524aa786bf5f17b4c68c7487b839
marisaballeani <83376531+marisaballeani@users.noreply.github.com>
2021-05-06 17:57:21 -0700

    Add files via upload

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -15,16 +15,24 @@ class ViewController: UIViewController {
     }
     
     let emojis = ["🥺": "sad", "🤩": "star"]
-     let customMessages = ["sad":["try again"], "star": ["you're a star"]]
-     @IBAction func showMessage(sender: UIButton) {
-         let selectedEmotion = (sender.titleLabel?.text)!
-         let alertController = UIAlertController(title: "Alert!", message: emojis[selectedEmotion], preferredStyle: UIAlertController.Style.alert)
-         let options = customMessages[emojis[selectedEmotion]!]!
-         let emojiMessage = options.randomElement()
+      let customMessages = ["sad":["try again"], "star": ["you're a star"]]
+       @IBAction func showMessage(sender: UIButton) {
+        if let selectedEmotion = (sender.titleLabel?.text){
+          let options = emojis[selectedEmotion]!
+          let emojiMessage = customMessages[emojis[selectedEmotion]!]![0]
+            
+            let selectedEmotion = (sender.titleLabel?.text)!
+            
+          let alertController = UIAlertController(title: "\(options)", message: "\(emojiMessage)", preferredStyle: UIAlertController.Style.alert)
+          alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+          present(alertController, animated: true, completion: nil)
+        }
+        /*
+         //let selectedEmotion = (sender.titleLabel?.text)!
+        let alertController = UIAlertController(title: "Alert!", message: emojis[selectedEmotion], preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
         _ = UIAlertController(title: "Alert!", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
-         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-         present(alertController, animated: true, completion: nil)
-        
-     }
-
-}
+        */
+       }
+    }
